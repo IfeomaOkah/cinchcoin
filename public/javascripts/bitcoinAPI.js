51b4b9b7ab5a7fa2640bba549a5aebadf530aca0
Ifeoma <ifeomaokah@Ifeomas-MacBook-Pro.local>
2019-08-02 11:24:42 +0200

    fixed small bug

--- public/javascripts/bitcoinAPI.js
+++ public/javascripts/bitcoinAPI.js
@@ -37,3 +37,43 @@ function dateSearch() {
   });
 });
 
+
+
+
+// Wallet Chart
+
+const ctx = document.getElementById('walletChart').getContext('2d');
+const walletChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Bitcoin', 'Ethereum', 'Litecoin', 'XRP'],
+        datasets: [{
+            label: 'Coins',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+               
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+                
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
